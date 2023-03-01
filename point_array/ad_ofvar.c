df9745643541fe89b43f49cb112806f72b66514d
uzochukwu-chinedu <chineduuzochukwu@gmail.com>
2023-03-01 14:30:37 -0800

    address of var

---
+++ point_array/ad_ofvar.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int i;
+	char j;
+
+	printf("The address of i is: %p\n", &i);
+	printf("The address of j is: %p\n", &j);
+	
+	return (0);
+}	
---
+++ point_array/size_of.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int i;
+	char chi = 2;
+
+	printf("Integer value is %lu\n", sizeof(int));
+	printf("character value is %lu\n", sizeof(char));
+	printf("float value is %lu\n", sizeof(float));
+	printf("Size of i is %lu\n", sizeof(i));
+	printf("size of chi is %lu\n", sizeof(chi));
+	
+	return (0);
+}
